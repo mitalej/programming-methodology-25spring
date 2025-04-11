d291cfb639261a0d9d09ddccb50401fe41a5b6b1
mita <mitalej@snu.ac.kr>
2025-04-11 11:54:01 +0900

    add final calculator_c file

--- calculator.cpp
+++ calculator.cpp
@@ -1,18 +1,44 @@
 #include "calculator.h"
 #include <stdexcept>
+#include <limits>
+
+using namespace std;
 
 int Calculator::add(int a, int b) {
-    // TODO
+    if ((b > 0) && (a > std::numeric_limits<int>::max() - b)) {
+        throw std::overflow_error("Add of");
+    }
+    if ((b < 0) && (a < std::numeric_limits<int>::min() - b)) {
+        throw std::overflow_error("Add uf");
+    }
+    return a + b;
 }
 
 int Calculator::sub(int a, int b) {
-    // TODO
+    if ((b < 0) && (a > std::numeric_limits<int>::max() + b)) {
+        throw std::overflow_error("Sub overflow");
+    }
+    if ((b > 0) && (a < std::numeric_limits<int>::min() + b)) {
+        throw std::overflow_error("Sub underflow");
+    }
+    return a - b;
 }
 
 int Calculator::mul(int a, int b) {
-    // TODO
+    if (a != 0 && b != 0) {
+        if (a > std::numeric_limits<int>::max() / b || a < std::numeric_limits<int>::min() / b) {
+            throw std::overflow_error("Mul overflow");
+        }
+    }
+    return a * b;
 }
 
 int Calculator::div(int a, int b) {
-    // TODO
+    if (b == 0) {
+        throw std::invalid_argument("Div by zero");
+    }
+    if (a == std::numeric_limits<int>::min() && b == -1) {
+        throw std::overflow_error("Div overflow");
+    }
+    return a / b;
 }
